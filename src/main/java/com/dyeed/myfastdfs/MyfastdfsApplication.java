02e5d77ef9cb740130e12351dc2242aa41ec2b28
Dyeed <gggssn@qq.com>
2020-06-01 17:33:30 +0800

    Initial commit

---
+++ src/main/java/com/dyeed/myfastdfs/MyfastdfsApplication.java
@@ -0,0 +1,13 @@
+package com.dyeed.myfastdfs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfastdfsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfastdfsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dyeed/myfastdfs/MyfastdfsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dyeed.myfastdfs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfastdfsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
